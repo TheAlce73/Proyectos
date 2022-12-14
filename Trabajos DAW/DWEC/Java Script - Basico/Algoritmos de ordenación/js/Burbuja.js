f26e32c2cc9547a3ccb01ab766d68cebc1dc9a43
TheAlce73 <49132422+TheAlce73@users.noreply.github.com>
2022-12-14 14:53:45 +0100

    Add files via upload

---
+++ Trabajos DAW/DWEC/Java Script - Basico/Algoritmos de ordenación/js/Burbuja.js
@@ -0,0 +1,39 @@
+var resultado;
+function Burbuja(){
+	
+	resultado=document.getElementById("Resultado");
+	var entrada=document.getElementById("numeros").value;
+	resultado.innerHTML="";
+	var entArray=entrada.split(",");
+
+	var i=0;
+	var ordenado=false;
+
+	while(i<entArray.length && ordenado!=true){
+		ordenado=true;
+		i++;
+		for (var j = 0; j < entArray.length; j++) {
+			if (parseInt(entArray[j])>parseInt(entArray[j+1])) {
+				console.log(entArray[j]+">"+entArray[j+1]);
+				ordenado=false;
+				var aux=entArray[j];
+				entArray[j]=entArray[j+1];
+				entArray[j+1]=aux;
+			}
+		}
+		
+	}
+	var vari=0;
+	while(vari<entArray.length){
+		if(vari==entArray.length-1){
+		resultado.innerHTML+=entArray[vari]+"  ";
+		}
+		else{
+			resultado.innerHTML+=entArray[vari]+" , ";
+		}
+		vari++;
+
+
+	}
+}
+window.onload=Burbuja;
