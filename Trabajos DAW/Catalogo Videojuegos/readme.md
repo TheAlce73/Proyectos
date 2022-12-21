52f2f4c219d89c3ecbcf8baaa49dc67a429814a4
TheAlce73 <49132422+TheAlce73@users.noreply.github.com>
2022-12-21 19:32:04 +0100

    1

---
+++ Trabajos DAW/Catalogo Videojuegos/readme.md
@@ -0,0 +1,3 @@
+# Catalogo Videojuegos
+
+> En este proyecto se mezcla php y JS. A traves de Js se controla el diseño de la página y gracias a php podemos usar la función añadir al carrito ya que guarda en una cookie si está guardado o no par en un futuro usarlo
---
+++ Trabajos DAW/Catalogo Videojuegos/videojuegos.js
@@ -0,0 +1,95 @@
+var datos;
+var idProdMostrado;
+var cesta=[];
+//para que cambie precio
+function atualizar(){
+ $.getJSON("videojuegos.php", function(data, status){
+    datos = data;
+  });
+
+}
+atualizar();
+
+ 
+$(document).ready(function (){
+	console.log("Inicio");
+	atualizar();
+	 window.alert(datos[0]);
+	$("#prodAnt").click(fProdAnt);
+	$("#prodSig").click(fProdSig);
+	$("#adquirir").click(anadir);	
+	$("#carrito").click(mostrar);
+	
+	idProdMostrado = 0;
+
+	rellenaDatos(idProdMostrado);
+
+	
+});
+
+function fProdAnt(){
+	atualizar();
+
+	idProdMostrado--;
+	if (idProdMostrado == -1) {
+		idProdMostrado = datos.length-1;
+	}
+	rellenaDatos(idProdMostrado);
+}
+
+function fProdSig(){
+	idProdMostrado++;
+	console.log(datos.length);
+	if (idProdMostrado == datos.length) {
+		idProdMostrado = 0;
+	}
+	rellenaDatos(idProdMostrado);
+}
+
+function rellenaDatos(X){
+
+	$("#img").attr("src",datos[X]["imagen"]);
+
+	$("#img1").attr("src",datos[X]["imagenes"][0]);
+	$("#img11").attr("src",datos[X]["imagenes"][0]);
+
+	$("#img2").attr("src",datos[X]["imagenes"][1]);
+
+	$("#img3").attr("src",datos[X]["imagenes"][2]);
+
+	$("#titulo").html(
+			'<h1>'+datos[X]["nombre"]+'</h1>');
+
+	$("#foto").html(
+			'<img src="'+datos[X]["imagen"]+'"></img>');
+
+	$("#texto").html(
+			'<p>'+datos[X]["def"]+'</p>');
+
+	var html="";
+
+	/*for (var i = datos[X]["caract"].length-1; 0<= i; i--) {
+			html+="<li>"+datos[X]["caract"][i]+"</li>";
+		}
+*/	
+}
+
+function cambiaPantalla(x){
+	$("#img1").attr("src",datos[idProdMostrado]["imagenes"][x]);
+}
+
+
+
+function anadir(){
+	console.log("añadido");
+	cesta.push(idProdMostrado);
+	$("#cesta").html("Cesta("+cesta.length+")");
+}
+
+function mostrar(){
+	var html2="";
+	for (var i = cesta.length-1; 0<= i; i--) {
+			html2+='<img src="'+datos[cesta[i]]["imagen"]+'"</img><hr>';
+		}
+	$("#productos").html(html2);
+}
